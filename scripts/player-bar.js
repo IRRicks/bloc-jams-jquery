8dbc7081c3c40056d45c6474c2e7d053cbe6ee33
Imani <imaniricks8@gmail.com>
2020-03-25 13:02:31 -0400

    checkpoint 17 completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -3,13 +3,23 @@
     player.playPause();
     $(this).attr('playState', player.playState);
   });
-  
+
   $('button#next').on('click', function() {
     if (player.playState !== 'playing') { return; }
 
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
     if (nextSongIndex >= album.songs.length) { return; }
+
+    $('button#previous').on('click', function() {
+      if (player.playState !== 'playing') { return; }
+
+      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+      const previousSongIndex = currentSongIndex - 1;
+      if (previousSongIndex >= album.songs.length) { return; }
+
+
+
 
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
