51f67726feee2a65415503163daf828073da36e5
Imani Ricks <53453645+IRRicks@users.noreply.github.com>
2020-03-28 21:28:05 -0400

    update checkpoint-18

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,9 +1,9 @@
 class Helper{
-  constructor(song){
-    this.song = song;
-  }
-  playPauseAndUpdate(song){
+  playPauseAndUpdate (song) {
     player.playPause(song);
+    const totalTime = player.currentlyPlaying.duration;
+    $('#time-control .total-time').text( totalTime );
   }
 }
-var helper = new Helper(song);
+
+const helper = new Helper();
